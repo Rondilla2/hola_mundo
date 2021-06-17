392c716321db57eeaba97115ee0159c3738dfc08
Rondilla2 <familiagilalonso@gmail.com>
2021-06-17 20:49:45 +0200

    Agregar archivos de proyecto.

---
+++ hola_mundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecucion hasta presionar tecla
+        }
+    }
+}
